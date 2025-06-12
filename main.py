8678e9b49f63df35214a44f4bd28eff6ebf5ccfb
jeison <jei_son1516@hotmail.com>
2025-06-12 01:10:22 -0400

    implement argparse to handle user prompt and verbose flag

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(
+    description="This cli app is my awesome gemini ai agent"
+)
+parser.add_argument("message", help="The message to be processed by gemini")
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="Enable verbose output"
+)
+
+args = parser.parse_args()
+message = [types.Content(role="user", parts=[types.Part(text=args.message)])]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=message,
 )
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if args.verbose:
+    print("User prompt:", args.message)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+print(response.text)
